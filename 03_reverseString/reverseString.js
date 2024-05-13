76a6a436719f145c6b98df928fdb327f800a251f
LeSparkly <harley.ray2000@gmail.com>
2024-05-13 14:19:40 -0400

    Nonworking solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let slicedLetter = '';
+    let reversedString = '';
+    let stringLength = string.length;
 
+    while(stringLength > 0) {
+        slicedLetter = slicedLetter.slice(stringLength)
+        reversedString = reversedString.concat(slicedLetter)
+        stringLength--
+    }
+    return reversedString;
 };
-
+reverseString("Hello")
 // Do not edit below this line
 module.exports = reverseString;
+5
