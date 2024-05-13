55c9944086e84716773e9d96247d33e7ee65a629
LeSparkly <harley.ray2000@gmail.com>
2024-05-12 23:45:28 -0400

    Added answer for helloWorld and repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  if (number < 0) {
+    return "ERROR";
+  }
+  let x = 0;
+  let repeatedString = "";
+  while (x < number) {
+    repeatedString = repeatedString.concat(string);
+    x++;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
